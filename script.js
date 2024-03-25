d123a02e3aca7477cda65c2b5d112f0d49d0c0a1
KushalMDev <105456987+KushalMDev@users.noreply.github.com>
2024-03-25 15:55:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+let input=document.querySelector("#fname")
+input.addEventListener("blur",()=>{capitalise(input.value)})
+function capitalise(str){
+    let res=str.toUpperCase()
+    // console.log(res);
+    input.value=res
+}
